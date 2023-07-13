18290faa0c7a425415a87873f416dee8788e1efe
g.nishi9525 <g.nishi9525@gmail.com>
2023-07-13 18:12:01 +0530

    made footer

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
         <Route path="/explore/:id" element={<Explore />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
